58dfc59fad7f499310fa1ef3381c786402d50b1f
sivm99 <git.v7gr4@simplelogin.fr>
2025-05-06 01:13:42 +0530

    Introduce Front end as well

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      APP
+      <Button>Dont Click me please</Button>
+    </>
+  );
+}
+
+export default App;
